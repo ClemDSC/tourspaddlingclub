fecf3dfe080d6a5cbc30336429bc3070356e256c
ClemDSC <clemence.p14@gmail.com>
2023-07-13 21:57:52 +0200

    page home: introduction

--- src/Components/home/SliderHome.jsx
+++ src/Components/home/SliderHome.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 import Slider from "react-slick";
-import { Box, Image } from '@chakra-ui/react'
+import { Box, Image } from "@chakra-ui/react";
 
-
-import Club from "../../Assets/club.jpg"
-import Chenonceau from "../../Assets/chenonceau.jpg"
-import Nature from "../../Assets/nature.jpg"
+import Club from "../../Assets/club.jpg";
+import Chenonceau from "../../Assets/chenonceau.jpg";
+import Nature from "../../Assets/nature.jpg";
+import Logo from "../../Assets/logo-tpc.png"
 
 export default function SliderHome() {
   const settings = {
@@ -15,22 +15,35 @@ export default function SliderHome() {
     autoplaySpeed: 3000,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
   };
 
   return (
-    <Box>
+    <Box position={"relative"}>
       <Slider {...settings}>
-          <div className='container'>
-            <Image objectFit='cover' src={Club} alt='Tours Paddling Club - club' />
-          </div>
-          <div className='container'>
-            <Image objectFit='cover' src={Chenonceau} alt='Tours Paddling Club - Chateau de Chenonceau' />
-          </div>
-          <div className='container'>
-            <Image objectFit='cover' src={Nature} alt='Tours Paddling Club - sortie nature' />
-          </div>
-        </Slider>
+        <div className="container">
+          <Image
+            objectFit="cover"
+            src={Club}
+            alt="Tours Paddling Club - club"
+          />
+        </div>
+        <div className="container">
+          <Image
+            objectFit="cover"
+            src={Chenonceau}
+            alt="Tours Paddling Club - Chateau de Chenonceau"
+          />
+        </div>
+        <div className="container">
+          <Image
+            objectFit="cover"
+            src={Nature}
+            alt="Tours Paddling Club - sortie nature"
+          />
+        </div>
+      </Slider>
+      <Image position={"absolute"} top={'50%'} left={'50%'} transform={'translate(-50%, -50%)'} width={{base:'140px', md:'300px'}} src={Logo} alt="Tours Paddling Club - logo" />
     </Box>
-  )
+  );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,68 @@
-import { Box, Heading, Text, Flex } from '@chakra-ui/react'
+import {
+  Box,
+  Heading,
+  Text,
+  Flex,
+  Image,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/react";
 
-import SliderHome from "../Components/home/SliderHome"
+import SliderHome from "../Components/home/SliderHome";
+import { Link } from "react-router-dom";
+
+import Picto1 from "../Assets/picto-nous-rejoindre.png";
+import Picto2 from "../Assets/picto-e.png";
 
 function Home() {
   return (
-    <Box as='section' bg="gray.50">
+    <Box as="section" bg="gray.50">
       <SliderHome />
-      <Flex flexDirection='column' as='article' p={4} gap={'4'}>
-        <Heading as='h2' align="center" pb="2">Bienvenue !</Heading>
-        <Text>L’association <Text as='b'>Tours Paddling Club</Text>, créée le 17 Février 2015, est basée à Tours (Indre-et-Loire - 37), et permet de regrouper les pratiquants de <Text as='i'>Stand Up Paddle</Text> (Sup individuel, Goliath, Dragon), et s'initier à de nouvelles pratiques de glisse (Wing, Bungee surf ...).</Text>
-        <Text>Notre but est de développer ses activités en Touraine, en faisant notamment découvrir notre passion à travers des animations, photos et vidéos .</Text>
-        <Text>Nous proposons également des initiations pour les débutants et pour les adhérents des entraînements loisirs et compétitions tout au long de l’année, ainsi que de nombreuses sorties en Touraine.</Text>
-        <Text as='b'>Venez nous rejoindre au Lac des Peupleraies à Tours le dimanche après-midi à partir de 14h !</Text>
+      <Flex flexDirection="column" as="article" p={4} gap={"4"}>
+        <Heading as="h2" align="center" pb="2">
+          Bienvenue !
+        </Heading>
+        <Text>
+          L’association <Text as="b">Tours Paddling Club</Text>, créée le 17
+          Février 2015, est basée à Tours (Indre-et-Loire - 37), et permet de
+          regrouper les pratiquants de <Text as="i">Stand Up Paddle</Text> (Sup
+          individuel, Goliath, Dragon), et s'initier à de nouvelles pratiques de
+          glisse (Wing, Bungee surf ...).
+        </Text>
+        <Text>
+          Notre but est de développer ses activités en Touraine, en faisant
+          notamment découvrir notre passion à travers des animations, photos et
+          vidéos .
+        </Text>
+        <Text>
+          Nous proposons également des initiations pour les débutants et pour
+          les adhérents des entraînements loisirs et compétitions tout au long
+          de l’année, ainsi que de nombreuses sorties en Touraine.
+        </Text>
+        <Text as="b">
+          Venez nous rejoindre au Lac des Peupleraies à Tours le dimanche
+          après-midi à partir de 14h !
+        </Text>
+      </Flex>
+      <Flex flexDirection="column" as="article" p={4} gap={"4"}>
+        <Heading as="h3" align="center" pb="2">
+          Le club
+        </Heading>
+        <Flex flexDirection={{base:'column', md:'row'}} gap={{base: 8, md:'none'}} justifyContent={{md:"space-evenly"}}>
+          <LinkBox as={'flex'} display={'flex'} flexDirection='column' alignItems={'center'} justifyContent={'center'}>
+            <Image src={Picto1} alt="pictogramme surf" width={"80px"} />
+            <LinkOverlay href='#'>Nous rejoindre</LinkOverlay>
+            <Text>Tarifs et bulletin d'adhésion au club</Text>
+          </LinkBox>
+          <LinkBox as={'flex'} display={'flex'} flexDirection='column' alignItems={'center'} justifyContent={'center'}>
+            <Image src={Picto2} alt="pictogramme surf" width={"80px"} />
+            <LinkOverlay href='#'>Boutique</LinkOverlay>
+            <Text>Accédez à la boutique du club</Text>
+          </LinkBox>
+        </Flex>
       </Flex>
     </Box>
-  )
+  );
 }
 
-export default Home
+export default Home;
